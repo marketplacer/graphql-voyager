6c203e954d977db39d5cbcb8a66adff9707db06d
Ellis Clayton <ellis@ellis.codes>
2020-09-30 11:12:32 +1000

    refactor: split JS to own file and add linting

---
+++ index.js
@@ -0,0 +1,17 @@
+function introspectionProvider (introspectionQuery) {
+  return fetch('schema.json')
+    .then((response) => {
+      if (response.status !== 200) throw new Error(response)
+
+      return response.text()
+    })
+    .then(JSON.parse)
+    .catch((e) => {
+      console.error('Something went wrong')
+      console.error(e)
+    })
+}
+
+GraphQLVoyager.init(document.getElementById('voyager'), {
+  introspection: introspectionProvider
+})
